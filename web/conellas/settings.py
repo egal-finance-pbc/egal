6e0e949b7cfa67577781ea4b2912e7a0c45b7dee
Omar Cornejo <omarkhd.mx@gmail.com>
2020-11-18 00:46:37 -0600

    django makestatic workflow integrated

--- web/conellas/settings.py
+++ web/conellas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
