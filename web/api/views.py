4a2e350b50f13f6f4957f0c7285ce7da88465c0e
Omar Cornejo <omarkhd.mx@gmail.com>
2021-02-10 01:17:10 -0500

    user signup view with serializer

--- web/api/views.py
+++ web/api/views.py
@@ -1,6 +1,10 @@
-from rest_framework.views import APIView
-from rest_framework.exceptions import APIException
 from rest_framework import status
+from rest_framework.exceptions import ParseError, APIException
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ledger.core import Gateway, LedgerError
+from . import serializers
 
 
 class Accounts(APIView):
@@ -8,5 +12,29 @@ class Accounts(APIView):
     View to manage a collection of accounts.
     """
 
-    def post(self, request, fmt=None):
-        raise APIException(code=status.HTTP_501_NOT_IMPLEMENTED)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ledger = Gateway()
+
+    def post(self, request):
+        payload = serializers.SignUp(data=request.data)
+        if not payload.is_valid():
+            raise ParseError()
+
+        try:
+            account = self.ledger.create_account(
+                first_name=payload.validated_data['first_name'],
+                last_name=payload.validated_data['last_name'],
+                username=payload.validated_data['username'],
+                password=payload.validated_data['password'],
+            )
+            return Response(data={
+                'id': account.id,
+                'public_key': account.public_key,
+                'username': account.user.username,
+                'first_name': account.user.first_name,
+                'last_name': account.user.last_name,
+            }, status=status.HTTP_201_CREATED)
+
+        except LedgerError as e:
+            raise APIException(detail=e.message, code=status.HTTP_500_INTERNAL_SERVER_ERROR)
