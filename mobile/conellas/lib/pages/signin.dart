0c93f7d87165e91a8f0f3966b50ec7c09c837fef
Omar Cornejo <omarkhd.mx@gmail.com>
2021-02-10 22:04:02 -0500

    login form example validation

--- mobile/conellas/lib/pages/signin.dart
+++ mobile/conellas/lib/pages/signin.dart
@@ -13,7 +13,7 @@ class _SignInPageState extends State<SignInPage> {
         title: Text('Sign In'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(20),
         child: ListView(
           children: <Widget>[
             Container(
@@ -28,39 +28,7 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  contentPadding: const EdgeInsets.all(15),
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  contentPadding: const EdgeInsets.all(15),
-                ),
-              ),
-            ),
-            Container(
-              height: 50,
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: RaisedButton(
-                textColor: Colors.white,
-                color: Colors.blue,
-                child: Text('Login'),
-                onPressed: () {},
-              ),
-            ),
+            LoginForm(),
             Container(
               child: Row(
                 children: <Widget>[
@@ -79,6 +47,78 @@ class _SignInPageState extends State<SignInPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
+
+class LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Missing username';
+                } else if (value.length > 150) {
+                  return 'Username length exceeded';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(),
+                contentPadding: const EdgeInsets.all(15),
+              ),
+            )
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            child: TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Missing password';
+                }
+                return null;
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+                contentPadding: const EdgeInsets.all(15),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+            width: double.infinity,
+            height: 60,
+            child: ElevatedButton(
+              onPressed: () {
+
+                if (_formKey.currentState.validate()) {
+                  Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Not implemented yet')));
+                }
+              },
+              child: Text('Login'),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
