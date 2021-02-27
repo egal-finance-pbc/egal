c99c0653659dae8abf0013a5a8e233a9cdc882b8
Omar Cornejo <omarkhd.mx@gmail.com>
2021-02-26 20:26:48 -0500

    home first code organizing attempt

--- mobile/conellas/lib/pages/home.dart
+++ mobile/conellas/lib/pages/home.dart
@@ -9,54 +9,73 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<Me> futureMe;
-  var api = API();
+  API api;
 
   @override
   void initState() {
     super.initState();
-    futureMe = api.me();
+    this.api = API();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
-        child: FutureBuilder<Me>(
-          future: futureMe,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListTile(
-                leading: IconButton(
-                  color: Colors.white,
-                  icon: Icon(Icons.qr_code),
-                  iconSize: 35,
-                  onPressed: () {},
-                ),
-                title: Text(
-                  snapshot.data.firstName + ' ' + snapshot.data.lastName,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-                subtitle: Text(
-                  snapshot.data.username,
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white70,
-                  ),
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
-        ),
+      body: Column(
+        children: [
+          this.headerContainer(),
+          this.balanceContainer(),
+          this.transactionsContainer(),
+        ],
       ),
     );
+  }
+
+  Widget headerContainer() {
+    var futureMe = api.me();
+    return Container(
+      color: Colors.blue,
+      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      child: FutureBuilder<Me>(
+        future: futureMe,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListTile(
+              leading: IconButton(
+                color: Colors.white,
+                icon: Icon(Icons.qr_code),
+                iconSize: 35,
+                onPressed: () {},
+              ),
+              title: Text(
+                snapshot.data.firstName + ' ' + snapshot.data.lastName,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                snapshot.data.username,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white70,
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+
+  Widget balanceContainer() {
+    return Container();
+  }
+
+  Widget transactionsContainer() {
+    return Container();
   }
 }
