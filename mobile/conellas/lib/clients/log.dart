88871caf7ebb03e2ec19e0cefdbfc7c9c067bd96
jorgeminguer16 <george_minguer@hotmail.com>
2021-03-29 17:52:30 -0500

    const name correction

--- mobile/conellas/lib/clients/log.dart
+++ mobile/conellas/lib/clients/log.dart
@@ -4,29 +4,29 @@ class Logger {
   final String name;
   Logger({this.name});
 
-  static const DEBUG = 100;
-  static const INFO = 200;
-  static const WARN = 300;
-  static const ERROR = 400;
-  static const FATAL = 500;
+  static const DebugLevel  = 100;
+  static const InfoLevel = 200;
+  static const WarnLevel = 300;
+  static const ErrorLevel = 400;
+  static const FatalLevel = 500;
 
   void debug(String message) {
-    dev.log(message, level: DEBUG, name: name);
+    dev.log(message, level: DebugLevel , name: name);
   }
 
   void info(String message) {
-    dev.log(message, level: INFO, name: name);
+    dev.log(message, level: InfoLevel, name: name);
   }
 
   void warn(String message) {
-    dev.log(message, level: WARN, name: name);
+    dev.log(message, level: WarnLevel, name: name);
   }
 
   void error(String message) {
-    dev.log(message, level: ERROR, name: name);
+    dev.log(message, level: ErrorLevel, name: name);
   }
 
   void fatal(String message) {
-    dev.log(message, level: FATAL, name: name);
+    dev.log(message, level: FatalLevel, name: name);
   }
 }
