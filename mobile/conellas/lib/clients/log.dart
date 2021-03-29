cd3c8061ade3dc6cf50855a8043acbd2b5ee8cc3
jorgeminguer16 <george_minguer@hotmail.com>
2021-03-29 17:43:28 -0500

    static const

--- mobile/conellas/lib/clients/log.dart
+++ mobile/conellas/lib/clients/log.dart
@@ -4,23 +4,29 @@ class Logger {
   final String name;
   Logger({this.name});
 
+  static const DEBUG = 100;
+  static const INFO = 200;
+  static const WARN = 300;
+  static const ERROR = 400;
+  static const FATAL = 500;
+
   void debug(String message) {
-    dev.log(message, level: 100, name: name);
+    dev.log(message, level: DEBUG, name: name);
   }
 
   void info(String message) {
-    dev.log(message, level: 200, name: name);
+    dev.log(message, level: INFO, name: name);
   }
 
   void warn(String message) {
-    dev.log(message, level: 300, name: name);
+    dev.log(message, level: WARN, name: name);
   }
 
   void error(String message) {
-    dev.log(message, level: 400, name: name);
+    dev.log(message, level: ERROR, name: name);
   }
 
   void fatal(String message) {
-    dev.log(message, level: 500, name: name);
+    dev.log(message, level: FATAL, name: name);
   }
 }
