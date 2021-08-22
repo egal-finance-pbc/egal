c3f4a4708e770f37b105ca5081b1e3ce5aae7f3f
Jorge Mingüer <45501275+jorgeminguer16@users.noreply.github.com>
2021-08-21 22:22:55 -0500

    adding date

--- web/api/views.py
+++ web/api/views.py
@@ -134,6 +134,7 @@ class Payments(APIView):
         return Response(data=[{
             'amount': str(p.amount),
             'description': p.description,
+            'date': p.date,
             'source': {
                 'username': p.source.user.username,
                 'first_name': p.source.user.first_name,
