213e1491a86de00a7685e3e31c33c1e2fb23ef0f
Jorge Mingüer <45501275+jorgeminguer16@users.noreply.github.com>
2021-09-13 11:06:34 -0500

    creating UserSerializer and AccountUpdateSerializer

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -33,3 +33,28 @@ class AccountQuerySerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         pass
+
+
+class UserSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=24)
+    last_name = serializers.CharField(max_length=24)
+    username = serializers.CharField(min_length=6, max_length=12)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+
+class AccountUpdateSerializer(serializers.Serializer):
+    phone = serializers.CharField(min_length=10, max_length=15)
+    photo = serializers.ImageField(max_length=254)
+    city = serializers.CharField(max_length=60)
+    country = serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
