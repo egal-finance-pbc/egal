a9e13e4f4dafdda9c5800a94bc2566ca8a7a3a17
Jorge Mingüer <45501275+jorgeminguer16@users.noreply.github.com>
2021-09-13 11:08:30 -0500

    add url update

--- web/api/urls.py
+++ web/api/urls.py
@@ -6,7 +6,9 @@ from . import views
 urlpatterns = [
     path('accounts/', views.Accounts.as_view()),
     path('accounts/<str:pubkey>/', views.Account.as_view()),
+    path('accounts/<str:pubkey>/update/', views.Account.as_view()),
     path('payments/', views.Payments.as_view()),
     path('tokens/', fviews.obtain_auth_token),
     path('me/', views.Me.as_view()),
 ]
+
