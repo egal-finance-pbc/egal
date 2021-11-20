4afd1f147313ab2e088cb4f5c32c78874d2d136c
Emir Blas <happytechuu@gmail.com>
2021-11-19 22:50:09 -0500

    nuevos cambios

--- mobile/conellas/lib/pages/sendSaving.dart
+++ mobile/conellas/lib/pages/sendSaving.dart
@@ -58,10 +58,10 @@ class _SendSavingState extends State<SendSaving> {
     var futureMe = widget.deps.api.me();
     var futureCountry = widget.deps.api.price();
     var futureBalance = widget.deps.api.account();
-    futureCountry.then((data) {
+    /*futureCountry.then((data) {
       price = data.rates.xlm;
       print(price);
-    });
+    });*/
 
     futureMe.then((data) {
       isoCode = data.country;
@@ -156,9 +156,7 @@ class _SendSavingState extends State<SendSaving> {
                                       crossAxisAlignment: CrossAxisAlignment.end,
                                       children: <Widget>[
                                         Text(
-                                          currency.format(this.balanceDouble *
-                                              20.0 *
-                                              this.price),
+                                          currency.format(this.balanceDouble),
                                           textAlign: TextAlign.center,
                                           style: TextStyle(
                                             fontSize: 45,
