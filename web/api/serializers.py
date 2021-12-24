b2c47368c937c36a2880a410b918707b8a779a68
Jorge Mingüer <45501275+jorgeminguer16@users.noreply.github.com>
2021-12-23 21:48:53 -0500

    agregando nombre y apellido

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 
 class SignUp(serializers.Serializer):
     names = serializers.CharField(max_length=24)
-    dad_last_name = serializers.CharField(max_length=24)
-    mom_last_name = serializers.CharField(max_length=24, required=False)
+    paternal_surname = serializers.CharField(max_length=24)
+    maternal_surname = serializers.CharField(max_length=24, required=False)
     username = serializers.CharField(min_length=6, max_length=12)
     password = serializers.CharField(min_length=8, max_length=12)
     phone = serializers.CharField(min_length=10, max_length=15)
