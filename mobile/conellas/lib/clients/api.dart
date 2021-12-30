cf5eb7bf4c8060c0699b198c1699ceb9cc3b28a3
AlexiSierra98 <alexisierra20998@gmail.com>
2021-12-30 13:08:04 -0500

    se comento la url de emir y se puso la general

--- mobile/conellas/lib/clients/api.dart
+++ mobile/conellas/lib/clients/api.dart
@@ -14,7 +14,10 @@ class API {
 
   API() {
     // TODO: Make base URL address:port dynamic.
-    this.url = 'http://192.168.0.112:5000/api/v1/';
+    // URL GENERAL PARA TODOS
+    this.url = 'http://10.0.2.2:5000/api/v1/';
+    // URL PARA PRUEBAS DE EMIR
+    //this.url = 'http://192.168.0.112:5000/api/v1/';
     this.urlStellar = 'http://api.coinlayer.com/api/live?access_key=';
   }
 
