a357c6cefc08f25149ee59105681a794fd6dae7e
Emir Blas <89540564+EmirBlas@users.noreply.github.com>
2022-05-30 18:53:31 -0500

    Update settings.py

--- web/conellas/settings.py
+++ web/conellas/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'conellas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Egal',
+        'NAME': 'egal',
         'USER': 'egalmaster',
         'PASSWORD': 'YDCbyLRrt0xJneD5',
         'HOST': 'awsegal.clu8p2ijbbzz.us-east-1.rds.amazonaws.com',
